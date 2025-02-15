6bceb16c4ef4ebe573a6d3abd4c417c79afbcd25
Grëg Häris <d.gregharis@gmail.com>
2025-02-15 21:19:06 +0100

    fix: bring back deleted types

--- types/index.ts
+++ types/index.ts
@@ -1,26 +1,42 @@
+// ====== USER PARAMS
+export type CreateUserParams = {
+  clerkId: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  image: string;
+};
+
+export type UpdateUserParams = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  image: string;
+};
+
 // ====== EVENT PARAMS
 export type CreateEventParams = {
   userId: string;
   event: {
     title: string;
-    description?: string;
-    location?: string;
+    description: string;
+    location: string;
     imageUrl: string;
     startDateTime: Date;
     endDateTime: Date;
     categoryId: string;
     price: string;
-    currency: string;
     isFree: boolean;
-    url?: string;
-    contactDetails: {
-      phoneNumber: string;
-      email: string;
-      website?: string;
-      instagram?: string;
-      facebook?: string;
-      x?: string;
-    };
+    url: string;
+  };
+  contactDetails: {
+    phoneNumber: string;
+    email: string;
+    website?: string;
+    instagram?: string;
+    facebook?: string;
+    x?: string;
   };
   path: string;
 };
@@ -30,40 +46,64 @@ export type UpdateEventParams = {
   event: {
     _id: string;
     title: string;
-    description?: string;
-    location?: string;
     imageUrl: string;
+    description: string;
+    location: string;
     startDateTime: Date;
     endDateTime: Date;
     categoryId: string;
     price: string;
-    currency: string;
     isFree: boolean;
-    url?: string;
-    contactDetails: {
-      phoneNumber: string;
-      email: string;
-      website?: string;
-      instagram?: string;
-      facebook?: string;
-      x?: string;
-    };
+    url: string;
+  };
+  contactDetails: {
+    phoneNumber: string;
+    email: string;
+    website?: string;
+    instagram?: string;
+    facebook?: string;
+    x?: string;
   };
   path: string;
+};
+
+export type DeleteEventParams = {
+  eventId: string;
+  path: string;
+};
+
+export type GetAllEventsParams = {
+  query: string;
+  category: string;
+  limit: number;
+  page: number;
+  location: string;
+};
+
+export type GetEventsByUserParams = {
+  userId: string;
+  limit?: number;
+  page: number;
+};
+
+export type GetRelatedEventsByCategoryParams = {
+  categoryId: string;
+  eventId: string;
+  limit?: number;
+  page: number | string;
 };
 
 export type Event = {
   _id: string;
   title: string;
-  description?: string;
+  description: string;
   price: string;
-  currency: string;
   isFree: boolean;
   imageUrl: string;
-  location?: string;
+  location: string;
   startDateTime: Date;
   endDateTime: Date;
-  url?: string;
+  url: string;
   organizer: {
     _id: string;
     firstName: string;
@@ -81,4 +121,54 @@ export type Event = {
     facebook?: string;
     x?: string;
   };
+};
+
+// ====== CATEGORY PARAMS
+export type CreateCategoryParams = {
+  categoryName: string;
+};
+
+// ====== ORDER PARAMS
+export type CheckoutOrderParams = {
+  eventTitle: string;
+  eventId: string;
+  price: string;
+  isFree: boolean;
+  buyerId: string;
+};
+
+export type CreateOrderParams = {
+  stripeId: string;
+  eventId: string;
+  buyerId: string;
+  totalAmount: string;
+  createdAt: Date;
+};
+
+export type GetOrdersByEventParams = {
+  eventId: string;
+  searchString: string;
+};
+
+export type GetOrdersByUserParams = {
+  userId: string | null;
+  limit?: number;
+  page: string | number | null;
+};
+
+// ====== URL QUERY PARAMS
+export type UrlQueryParams = {
+  params: string;
+  key: string;
+  value: string | null;
+};
+
+export type RemoveUrlQueryParams = {
+  params: string;
+  keysToRemove: string[];
+};
+
+export type SearchParamProps = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
