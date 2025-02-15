4daed9948f8167ed929412d56660e42531735195
Grëg Häris <d.gregharis@gmail.com>
2025-02-15 19:06:32 +0100

    feat: add contact details

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,4 +1,3 @@
-
 import { Document, Schema, model, models } from 'mongoose';
 
 export interface IEvent extends Document {
@@ -16,23 +15,40 @@ export interface IEvent extends Document {
   url?: string;
   category: { _id: string; name: string };
   organizer: { _id: string; firstName: string; lastName: string };
+  contactDetails: {
+    phoneNumber: string;
+    email: string;
+    website?: string;
+    instagram?: string;
+    facebook?: string;
+    x?: string;
+  };
 }
 
-const EventSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  location: { type: String },
-  imageUrl: { type: String, required: true },
-  startDateTime: { type: Date, required: true, default: Date.now },
-  endDateTime: { type: Date, required: true, default: Date.now },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  price: { type: Number },
-  isFree: { type: Boolean, default: false },
-  url: { type: String },
-  category: { type: Schema.Types.ObjectId, ref: 'Category' },
-  organizer: { type: Schema.Types.ObjectId, ref: 'User' },
-});
+const EventSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    location: { type: String },
+    imageUrl: { type: String, required: true },
+    startDateTime: { type: Date, required: true, default: Date.now },
+    endDateTime: { type: Date, required: true, default: Date.now },
+    price: { type: Number },
+    isFree: { type: Boolean, default: false },
+    url: { type: String },
+    category: { type: Schema.Types.ObjectId, ref: 'Category' },
+    organizer: { type: Schema.Types.ObjectId, ref: 'User' },
+    contactDetails: {
+      phoneNumber: { type: String, required: true },
+      email: { type: String, required: true },
+      website: { type: String },
+      instagram: { type: String },
+      facebook: { type: String },
+      x: { type: String },
+    },
+  },
+  { timestamps: true }
+);
 
 const Event = models.Event || model<IEvent>('Event', EventSchema);
 
