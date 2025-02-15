fafdb251c6b08df618d3a5c29d48534064f16aa6
Grëg Häris <d.gregharis@gmail.com>
2025-02-16 00:39:36 +0100

    feat: add Admin, user/signIn buttons as components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
-import { Header } from '@shared/Header';
-import { Footer } from '@shared/Footer';
+import Header from '@shared/Header';
+import Footer from '@shared/Footer';
+import DisplayAdmin from './admin/displayAdminButton';
+import MobileNavAndUserButton from '@/components/shared/MobilNavAndUserButtons';
 
 export default function RootLayout({
   children,
@@ -7,9 +9,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className='flex flex-col h-screen'>
-      <Header />
-      <main className='flex-1'>{children}</main>
+    <div className="flex flex-col h-screen">
+      <div className='flex items-center justify-between'>
+        <Header /> <div className='whitespace-nowrap flex items-center justify-end'> <DisplayAdmin /> <MobileNavAndUserButton/></div>
+      </div>
+      <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
