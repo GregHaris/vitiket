9b2d62e4fe954d7aa1407bb72765fef8da0f20dd
Grëg Häris <d.gregharis@gmail.com>
2025-02-15 19:59:46 +0100

    feat: get user roles and metadata

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,9 +1,13 @@
 export {};
 
+export type Roles = 'admin'
 declare global {
   interface CustomJwtSessionClaims {
     userId?: {
       userId?: string;
+    };
+    metadata: {
+      role?: Roles;
     };
   }
 }
