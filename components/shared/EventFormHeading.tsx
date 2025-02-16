f63c79330455b1d02d16a4c13a6e087e39952ab4
Grëg Häris <d.gregharis@gmail.com>
2025-02-16 17:14:49 +0100

    fix: modify paragraph

--- components/shared/EventFormHeading.tsx
+++ components/shared/EventFormHeading.tsx
@@ -15,7 +15,7 @@ const EventFormHeading = () => {
     <div className="wrapper text-left">
       <h3 className=" h3-bold">Hey {userName}!</h3>
       <p className="p-regular-16 py-4">
-        Ready to add your product? It only takes a few minutes
+        Ready to create your event? It only takes a few minutes
       </p>
     </div>
   );
