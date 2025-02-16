03c6c09e366e3aaebeab9783a0d646ff3e995859
Grëg Häris <d.gregharis@gmail.com>
2025-02-16 14:20:06 +0100

    fix: alliase Button import

--- components/shared/DisplayAdminButton.tsx
+++ components/shared/DisplayAdminButton.tsx
@@ -1,5 +1,5 @@
 import { checkRole } from '@/utils/roles';
-import { Button } from '@/components/ui/button';
+import { Button } from '@ui/button';
 import Link from 'next/link';
 
 const DisplayAdmin = async () => {
