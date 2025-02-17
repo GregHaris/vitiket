8bf525fb1ba4e3a49f09c27d74ce58112ff08dcb
Grëg Häris <d.gregharis@gmail.com>
2025-02-17 21:37:55 +0100

    feat: add types input dropdown

---
+++ components/shared/TypesDropdown.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { getAllTypes } from '@/lib/actions/type.actions';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { DropdownProps } from '@/types';
+
+const TypesDropdown = ({ value, onChange }: DropdownProps) => {
+  const [types, setTypes] = useState<{ _id: string; name: string }[]>([]);
+
+  useEffect(() => {
+    const fetchTypes = async () => {
+      const types = await getAllTypes();
+      setTypes(types);
+    };
+
+    fetchTypes();
+  }, []);
+
+  return (
+    <Select onValueChange={onChange} defaultValue={value}>
+      <SelectTrigger className="select-field p-regular-14">
+        <SelectValue placeholder="Select a type" />
+      </SelectTrigger>
+      <SelectContent>
+        {types.map((type) => (
+          <SelectItem key={type._id} value={type._id}>
+            {type.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default TypesDropdown;
