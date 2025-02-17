bb9d5c47475b6fe108a1d02a8f3938ee5c1ab294
Grëg Häris <d.gregharis@gmail.com>
2025-02-17 21:50:41 +0100

    style: reduce th text size of the start and end date & time input

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -371,7 +371,7 @@ export default function EventForm({
                         <span className="text-red-400">*</span>
                       </FormLabel>
                       <FormControl>
-                        <div className="flex-center h-[40px] w-full overflow-hidden rounded-md border-gray-300 border bg-grey-50 px-4 py-2">
+                        <div className="flex-center h-[40px] w-full overflow-hidden rounded-md border-gray-300 border bg-grey-50 px-4 py-2 text-sm">
                           <Image
                             src="/assets/icons/calendar.svg"
                             width={20}
@@ -408,7 +408,7 @@ export default function EventForm({
                         End Date & Time <span className="text-red-400">*</span>
                       </FormLabel>
                       <FormControl>
-                        <div className="flex-center h-[40px] w-full overflow-hidden rounded-md border-gray-300 border bg-grey-50 px-4 py-2">
+                        <div className="flex-center h-[40px] w-full overflow-hidden rounded-md border-gray-300 border text-sm bg-grey-50 px-4 py-2">
                           <Image
                             src="/assets/icons/calendar.svg"
                             width={20}
