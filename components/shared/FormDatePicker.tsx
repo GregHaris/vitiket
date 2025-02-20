ac2c061aeb5351a2d6820db053f3cc8f72fdfdd8
Grëg Häris <d.gregharis@gmail.com>
2025-02-20 10:49:04 +0100

    fix: replace react-hook-form Controller with shadCn Form component module

--- components/shared/FormDatePicker.tsx
+++ components/shared/FormDatePicker.tsx
@@ -1,37 +1,51 @@
-import { Controller, useFormContext } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 import DatePicker from 'react-datepicker';
 import Image from 'next/image';
 
 import { DateTimePickerProps } from '@/types';
 
 import 'react-datepicker/dist/react-datepicker.css';
+import { FormControl, FormField, FormItem, FormLabel } from '../ui/form';
 
-export default function DateTimePicker({ name, label }: DateTimePickerProps) {
+export default function DateTimePicker({
+  name,
+  label,
+  placeholder,
+}: DateTimePickerProps) {
   const { control } = useFormContext();
 
   return (
-    <Controller
+    <FormField
       control={control}
       name={name}
       render={({ field }) => (
-        <div className="flex-center h-[40px] w-full overflow-hidden rounded-md border-gray-300 border bg-grey-50 px-4 py-2 text-sm">
-          <Image
-            src="/assets/icons/calendar.svg"
-            width={20}
-            height={20}
-            alt="calendar"
-            className="filter-grey"
-          />
-          <p className="ml-3 whitespace-nowrap text-gray-600">{label}</p>
-          <DatePicker
-            selected={field.value}
-            onChange={(date: Date | null) => field.onChange(date)}
-            showTimeSelect
-            timeInputLabel="Time:"
-            dateFormat="dd/MM/yyyy h:mm aa"
-            wrapperClassName="datePicker"
-          />
-        </div>
+        <FormItem>
+          <FormLabel className="text-sm font-medium">
+            {label} <span className="text-red-400">*</span>
+          </FormLabel>
+          <FormControl>
+            <div className="flex-center h-[40px] w-full overflow-hidden rounded-md border-gray-300 border bg-grey-50 px-4 py-2 text-sm">
+              <Image
+                src="/assets/icons/calendar.svg"
+                width={20}
+                height={20}
+                alt="calendar"
+                className="filter-grey"
+              />
+              <p className="ml-3 whitespace-nowrap text-gray-600">
+                {placeholder}
+              </p>
+              <DatePicker
+                selected={field.value}
+                onChange={(date: Date | null) => field.onChange(date)}
+                showTimeSelect
+                timeInputLabel="Time:"
+                dateFormat="dd/MM/yyyy h:mm aa"
+                wrapperClassName="datePicker"
+              />
+            </div>
+          </FormControl>
+        </FormItem>
       )}
     />
   );
