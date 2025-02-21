3c5796d86bf9560d25e1d8e5f81c8eac75db305e
Grëg Häris <d.gregharis@gmail.com>
2025-02-21 13:37:46 +0100

    feat: rename event location types from "Online and In-Person" to "Virtual and Physical"

--- components/shared/FormLocationSection.tsx
+++ components/shared/FormLocationSection.tsx
@@ -15,7 +15,7 @@ export default function LocationSection({
 }: LocationProps) {
   return (
     <>
-      {(locationType === 'In-Person' || locationType=== 'Hybrid') &&   (
+      {(locationType === 'Physical' || locationType === 'Hybrid') && (
         <>
           <FormField
             control={control}
