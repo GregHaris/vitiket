a531a7fc73bac377b14d04054824b68c379867de
Grëg Häris <d.gregharis@gmail.com>
2025-02-22 16:22:51 +0100

    Feat: AddImage accepts the added file and pass it to the Parent EventForm Component

---
+++ components/shared/FormAddImageSection.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useFormContext } from 'react-hook-form';
+
+import { AddImageProps } from '@/types';
+import { eventFormValues } from '@/lib/validator';
+import FileUploader from './FileUploader';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@ui/form';
+
+export default function AddImage({ setFiles }: AddImageProps) {
+  const { control } = useFormContext<eventFormValues>();
+
+  return (
+    <FormField
+      control={control}
+      name="imageUrl"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="text-sm font-medium">
+            Image <span className="text-red-400">*</span>
+          </FormLabel>
+          <FormControl>
+            <FileUploader
+              imageUrl={field.value}
+              onFieldChange={field.onChange}
+              setFiles={setFiles}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
