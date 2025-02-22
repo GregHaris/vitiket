4e77a879023a73a2a66bd223d057a7d58868aab7
Grëg Häris <d.gregharis@gmail.com>
2025-02-22 17:41:55 +0100

    Feat: Seperate event start and end date and time intoSeperate compents

--- components/shared/FormTimePicker.tsx
+++ components/shared/FormTimePicker.tsx
@@ -2,17 +2,17 @@ import { useFormContext } from 'react-hook-form';
 import DatePicker from 'react-datepicker';
 import Image from 'next/image';
 
-import { DatePickerProps } from '@/types';
+import { TimePickerProps } from '@/types';
 import { eventFormValues } from '@/lib/validator';
 import { FormControl, FormField, FormItem, FormLabel } from '@ui/form';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
-export default function DatePickerComponent({
+export default function TimePickerComponent({
   name,
   label,
   placeholder,
-}: DatePickerProps) {
+}: TimePickerProps) {
   const { control } = useFormContext<eventFormValues>();
 
   return (
@@ -27,10 +27,10 @@ export default function DatePickerComponent({
           <FormControl>
             <div className="flex-center h-[40px] w-full overflow-hidden rounded-md border-gray-300 border bg-grey-50 px-4 py-2 text-sm">
               <Image
-                src="/assets/icons/calendar.svg"
+                src="/assets/icons/clock.svg"
                 width={20}
                 height={20}
-                alt="calendar"
+                alt="clock"
                 className="filter-grey"
               />
               <p className="ml-3 whitespace-nowrap text-gray-600">
@@ -39,7 +39,11 @@ export default function DatePickerComponent({
               <DatePicker
                 selected={field.value as Date}
                 onChange={(date: Date | null) => field.onChange(date)}
-                dateFormat="dd/MM/yyyy"
+                showTimeSelect
+                showTimeSelectOnly
+                timeIntervals={15}
+                timeCaption="Time"
+                dateFormat="h:mm aa"
                 wrapperClassName="datePicker"
               />
             </div>
