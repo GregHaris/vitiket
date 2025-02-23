bc429d59473c8c0799a14523a7a1578507eb8a57
Grëg Häris <d.gregharis@gmail.com>
2025-02-23 23:51:28 +0100

    feat: add "Host: " before host Full Name

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -80,7 +80,10 @@ const Card = async ({ event, hasOrderLink, hidePrice }: CardProps) => {
 
         <div className="flex-between w-full">
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
-            {event.organizer.firstName} {event.organizer.lastName}
+            <span className="font-bold">Host:</span>{' '}
+            <span>
+              {event.organizer.firstName} {event.organizer.lastName}
+            </span>
           </p>
           {hasOrderLink && (
             <Link href={`/orders?eventId=${event._id}`} className="flex gap-2">
