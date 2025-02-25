66f9f2dce9fef4e7d0f502f1dbbd6640ca3a3f3b
Grëg Häris <d.gregharis@gmail.com>
2025-02-25 15:03:25 +0100

    feat: make the checkout button sticky

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -34,7 +34,7 @@ const EventDetails = async (props: SearchParamProps) => {
 
   return (
     <>
-      <section className="wrapper">
+      <section className="wrapper pb-20 md:pb-0">
         <div className="2xl:max-w-7xl space-y-6 w-full">
           {/* Event Image */}
           <div className="w-full">
@@ -46,125 +46,133 @@ const EventDetails = async (props: SearchParamProps) => {
               className="min-h-[300px] md:h-[600px] w-full object-cover object-center rounded-2xl"
             />
           </div>
-
           <div className="flex w-full flex-col gap-8 p-5 md:p-10">
             <div className="flex flex-col gap-2 space-y-6">
               <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500 w-fit">
                 {event.category?.name}
               </p>
-
-              <h2 className="text-4xl font-bold">{event.title}</h2>
-              <div className="flex flex-col gap-5">
-                <div>
+              <div className="flex justify-between">
+                <div className="flex flex-col gap-5">
+                  <h2 className="text-4xl font-bold">{event.title}</h2>
                   <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
                     <span className="font-bold">Host:</span>{' '}
                     <span>
                       {event.organizer?.firstName} {event.organizer?.lastName}
                     </span>
                   </p>
-                </div>
-                <div className="flex gap-2 md:gap-3 items-center">
-                  <Image
-                    src={'/assets/icons/calendar.svg'}
-                    alt="calendar"
-                    width={32}
-                    height={32}
-                  />
-                  <div className="p-medium-16 lg:p-regular-20 items-center">
-                    {/* Conditionally render the date */}
-                    {isSameDate ? (
-                      <p>{formatDateTime(event.startDate).dateOnly}</p>
-                    ) : (
-                      <p>
-                        {formatDateTime(event.startDate).dateOnly} -{' '}
-                        {formatDateTime(event.endDate).dateOnly}
-                      </p>
-                    )}
-                  </div>
-                </div>{' '}
-                <div className="flex gap-2 md:gap-3 items-center">
-                  <Image
-                    src={'/assets/icons/clock.svg'}
-                    alt="calendar"
-                    width={32}
-                    height={32}
-                  />
-                  <div className="p-medium-16 lg:p-regular-20 items-center">
-                    <p>
-                      {formatDateTime(event.startTime).timeOnly} -{' '}
-                      {formatDateTime(event.endTime).timeOnly}
-                    </p>
-                  </div>
-                </div>
-                <div className="p-regular-20 flex item-center gap-3">
-                  <Image
-                    src={'/assets/icons/location.svg'}
-                    alt="location"
-                    width={32}
-                    height={32}
-                  />
-                  <p className="p-medium-16 lg:p-regular-20">
-                    {event.location}
-                  </p>
-                </div>
-              </div>
-              <div className="flex flex-col gap-2 space-y-6">
-                <h3 className="text-2xl font-bold">About this Event</h3>
-                <SafeHTMLRenderer html={event.description} />
-                <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
-                  {event.url}
-                </p>
-              </div>
-              <div
-                id="price-section"
-                className="flex flex-col gap-3 sm:flex-row sm:items-center"
-              >
-                <div className="flex gap-3">
-                  {/* Display Event Price or Price Categories */}
-                  {event.isFree ? (
-                    <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
-                      FREE
-                    </p>
-                  ) : event.priceCategories &&
-                    event.priceCategories.length === 1 ? (
-                    <div className="flex flex-col gap-2">
-                      <p className="p-bold-20">Price: </p>
-                      <p className="p-medium-16 px-4 py-2">
-                        {event.priceCategories[0].name} - {currencySymbol}
-                        {event.priceCategories[0].price}
-                      </p>
-                    </div>
-                  ) : (
-                    <div className="flex flex-col gap-2">
-                      <p className="p-bold-20">Price Categories</p>
-                      {event.priceCategories?.map(
-                        (category: PriceCategory, index: number) => (
-                          <p key={index} className="p-medium-16 px-4 py-2">
-                            {category.name}: {currencySymbol}
-                            {category.price}
-                          </p>
-                        )
+                  <div className="flex gap-2 md:gap-3 items-center">
+                    <Image
+                      src={'/assets/icons/calendar.svg'}
+                      alt="calendar"
+                      width={32}
+                      height={32}
+                    />
+                    <div className="p-medium-16 lg:p-regular-20 items-center">
+                      {/* Conditionally render the date */}
+                      {isSameDate ? (
+                        <p>{formatDateTime(event.startDate).dateOnly}</p>
+                      ) : (
+                        <p>
+                          {formatDateTime(event.startDate).dateOnly} -{' '}
+                          {formatDateTime(event.endDate).dateOnly}
+                        </p>
                       )}
                     </div>
-                  )}
+                  </div>
+
+                  <div className="flex gap-2 md:gap-3 items-center">
+                    <Image
+                      src={'/assets/icons/clock.svg'}
+                      alt="calendar"
+                      width={32}
+                      height={32}
+                    />
+                    <div className="p-medium-16 lg:p-regular-20 items-center">
+                      <p>
+                        {formatDateTime(event.startTime).timeOnly} -{' '}
+                        {formatDateTime(event.endTime).timeOnly}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="p-regular-20 flex item-center gap-3">
+                    <Image
+                      src={'/assets/icons/location.svg'}
+                      alt="location"
+                      width={32}
+                      height={32}
+                    />
+                    <p className="p-medium-16 lg:p-regular-20">
+                      {event.location}
+                    </p>
+                  </div>
+
+                  <div className="flex flex-col gap-2 space-y-6">
+                    <h3 className="text-2xl font-bold">About this Event</h3>
+                    <SafeHTMLRenderer html={event.description} />
+                    <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
+                      {event.url}
+                    </p>
+                  </div>
+
+                  <div
+                    id="price-section"
+                    className="flex flex-col gap-3 sm:flex-row sm:items-center"
+                  >
+                    <div className="flex gap-3">
+                      {/* Display Event Price or Price Categories */}
+                      {event.isFree ? (
+                        <p className="p-bold-20 rounded-md bg-green-500/10 px-5 py-2 text-green-700">
+                          FREE
+                        </p>
+                      ) : event.priceCategories &&
+                        event.priceCategories.length === 1 ? (
+                        <div className="flex flex-col gap-2">
+                          <p className="p-bold-20">Price: </p>
+                          <p className="p-medium-16 px-4 py-2">
+                            {event.priceCategories[0].name} - {currencySymbol}
+                            {event.priceCategories[0].price}
+                          </p>
+                        </div>
+                      ) : (
+                        <div className="flex flex-col gap-2">
+                          <p className="p-bold-20">Price Categories</p>
+                          {event.priceCategories?.map(
+                            (category: PriceCategory, index: number) => (
+                              <p key={index} className="p-medium-16 px-4 py-2">
+                                {category.name}: {currencySymbol}
+                                {category.price}
+                              </p>
+                            )
+                          )}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+
+                  {/* Map Section */}
+                  {(event.locationType === 'Physical' ||
+                    event.location === 'Hybrid') &&
+                    event.coordinates && (
+                      <div>
+                        <h3 className="text-2xl font-bold">Direction</h3>
+                        <div className="w-full h-[400px]">
+                          <EventMap
+                            coordinates={event.coordinates}
+                            destinationName={event.location}
+                          />
+                        </div>
+                      </div>
+                    )}
+                </div>
+
+                {/* {sticky button checkout button} */}
+                <div>
+                  <CheckoutButton event={event} />
                 </div>
               </div>
+              ;
             </div>
-            <CheckoutButton event={event} />
-            {/* Map Section */}
-            {(event.locationType === 'Physical' ||
-              event.location === 'Hybrid') &&
-              event.coordinates && (
-                <div>
-                  <h3 className="text-2xl font-bold">Direction</h3>
-                  <div className="w-full h-[400px]">
-                    <EventMap
-                      coordinates={event.coordinates}
-                      destinationName={event.location}
-                    />
-                  </div>
-                </div>
-              )}
           </div>
         </div>
       </section>
