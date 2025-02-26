210a9927d3c6628f6943c4f7b50b9bf8b2021fe4
Grëg Häris <d.gregharis@gmail.com>
2025-02-26 03:11:32 +0100

    feat: create a reusable module to provide the card display UI for the price types

---
+++ components/shared/PriceCardUI.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { Minus, Plus } from 'lucide-react';
+import { PriceCardUIProps } from '@/types';
+
+const PriceCard = ({
+  title,
+  price,
+  quantity,
+  onDecrease,
+  onIncrease,
+  currencySymbol = '',
+}: PriceCardUIProps) => {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm">
+      <h3 className="text-xl font-bold mb-4 text-gray-500">{title}</h3>
+      <div className="flex items-center justify-between">
+        <p className="text-sm text-gray-500 font-semibold">
+          {currencySymbol}
+          {price}
+        </p>
+        <div className="flex items-center gap-2">
+          <button
+            className="bg-gray-200 px-3 py-1 rounded cursor-pointer"
+            onClick={onDecrease}
+          >
+            <Minus className="text-sm text-gray-400 font-bold" />
+          </button>
+          <span>{quantity}</span>
+          <button
+            className="bg-primary px-3 py-1 rounded cursor-pointer"
+            onClick={onIncrease}
+          >
+            <Plus className="text-sm text-white font-bold" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PriceCard;
