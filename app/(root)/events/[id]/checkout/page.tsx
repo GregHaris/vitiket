2ae31d23e84bc98f401780ba704355d84c801915
Grëg Häris <d.gregharis@gmail.com>
2025-02-27 16:27:24 +0100

    feat: add checkout page to display the checkout details

---
+++ app/(root)/events/[id]/checkout/page.tsx
@@ -0,0 +1,18 @@
+import { getEventById } from '@/lib/actions/event.actions';
+import CheckoutDetails from '@shared/checkoutDetails';
+
+export default async function Checkout({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { quantity: string; totalPrice: string };
+}) {
+  const event = await getEventById(params.id);
+  const quantity = parseInt(searchParams.quantity);
+  const totalPrice = parseFloat(searchParams.totalPrice);
+
+  return (
+    <CheckoutDetails event={event} quantity={quantity} totalPrice={totalPrice} />
+  );
+}
