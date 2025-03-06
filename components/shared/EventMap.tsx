1d620f37ab56ab5058d37317d82df1cd3c399255
Grëg Häris <d.gregharis@gmail.com>
2025-03-06 23:49:22 +0100

    feat: get place details from google map

--- components/shared/EventMap.tsx
+++ components/shared/EventMap.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import { FC, memo, useEffect, useMemo, useState } from 'react';
 import {
   APIProvider,
-  Map,
   AdvancedMarker,
+  Map,
   Pin,
+  useMap,
+  useAdvancedMarkerRef,
+  useMapsLibrary,
 } from '@vis.gl/react-google-maps';
-import { useMemo } from 'react';
 
 import { EventMapProps } from '@/types';
 
@@ -15,39 +18,97 @@ const containerStyle = {
   height: '400px',
 };
 
-export default function EventMap({
-  coordinates,
-  destinationName,
-}: EventMapProps) {
-  const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!;
-
-  const [lat, lng] = coordinates.split(',').map(Number);
-  const center = useMemo(() => ({ lat, lng }), [lat, lng]);
-
+const EventMap: FC<EventMapProps> = ({ coordinates, destinationInfo }) => {
   return (
-    <APIProvider apiKey={API_KEY}>
-      <div className="relative">
-        {/* Google Map */}
-        <Map
-          mapId="a16cb905c97a2f28"
-          style={containerStyle}
-          defaultCenter={center}
-          defaultZoom={14}
-        >
-          {/* Advanced Marker */}
-          <AdvancedMarker
-            position={center}
-            clickable={true}
-            title={destinationName}
-          >
-            <Pin
-              background={'#EA4335'}
-              glyphColor={'#FFF'}
-              borderColor={'#EA4335'}
-            />
-          </AdvancedMarker>
-        </Map>
-      </div>
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
+      <EventMapContent
+        coordinates={coordinates}
+        destinationInfo={destinationInfo}
+      />
     </APIProvider>
   );
+};
+
+interface MapContentProps {
+  destinationInfo: EventMapProps['destinationInfo'];
+  coordinates: EventMapProps['coordinates'];
 }
+
+const EventMapContent: FC<MapContentProps> = ({
+  coordinates,
+  destinationInfo,
+}) => {
+  const [lat, lng] = coordinates.split(',').map(Number);
+  const center = useMemo(() => ({ lat, lng }), [lat, lng]);
+  const [address, locationMapId] = destinationInfo.split(', ||');
+
+  const [markerRef, marker] = useAdvancedMarkerRef();
+
+  const map = useMap();
+  const places = useMapsLibrary('places');
+
+  const [placesService, setPlacesService] =
+    useState<google.maps.places.PlacesService | null>(null);
+  const [placeDetails, setPlaceDetails] =
+    useState<google.maps.places.PlaceResult | null>(null);
+
+  // Initialize PlacesService when the Places library and map are available
+  useEffect(() => {
+    if (!places || !map) return;
+
+    const service = new places.PlacesService(map);
+    setPlacesService(service);
+  }, [places, map]);
+
+  // Fetch place details when PlacesService and locationMapId are available
+  useEffect(() => {
+    if (!placesService || !locationMapId?.trim()) return;
+
+    const request = {
+      placeId: locationMapId.trim(),
+      fields: ['name', 'formatted_address', 'geometry', 'place_id'],
+    };
+
+    placesService.getDetails(request, (place, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK && place) {
+        setPlaceDetails(place);
+
+        if (marker) {
+          marker.position = { lat, lng };
+        }
+
+        if (map) {
+          map.panTo({ lat, lng });
+        }
+      }
+    });
+  }, [placesService, map, marker, locationMapId]);
+
+  return (
+    <div className="relative">
+      {/* Google Map */}
+      <Map
+        mapId="a16cb905c97a2f28"
+        style={containerStyle}
+        defaultCenter={center}
+        defaultZoom={14}
+      >
+        {/* Advanced Marker */}
+        <AdvancedMarker
+          ref={markerRef}
+          position={center}
+          clickable={true}
+          title={placeDetails?.name || address}
+        >
+          <Pin
+            background={'#EA4335'}
+            glyphColor={'#FFF'}
+            borderColor={'#EA4335'}
+          />
+        </AdvancedMarker>
+      </Map>
+    </div>
+  );
+};
+
+export default memo(EventMap);
