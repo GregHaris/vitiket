5ff39b93c51f0419ded4e0a696696cf3f8e55599
Grëg Häris <d.gregharis@gmail.com>
2025-03-09 01:01:25 +0100

    feat: replace stripeAccountId with paystackAccountId

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -2,7 +2,7 @@ import { Schema, model, models } from 'mongoose';
 
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
-  stripeAccountId: { type: String },
+  paystackAccountId: { type: String },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
