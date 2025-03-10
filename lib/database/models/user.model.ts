b9497ce09beaa1fcac3ea1563b35c90e90524be3
Grëg Häris <d.gregharis@gmail.com>
2025-03-10 06:47:04 +0100

    feat: updaate user model-
    add bank details and sub account code

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,8 +1,28 @@
 import { Schema, model, models } from 'mongoose';
 
+export interface IUser {
+  _id: string;
+  clerkId: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  image: string;
+  createdAt: Date;
+  updatedAt: Date;
+  role: string;
+  businessName?: string;
+  subaccountCode?: string;
+  bankName?: string;
+  bankDetails?: {
+    bankCode?: string;
+    accountNumber?: string;
+  };
+}
+
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
-  paystackAccountCode: { type: String },
+  subaccountCode: { type: String },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
@@ -11,9 +31,11 @@ const UserSchema = new Schema({
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   role: { type: String, default: 'user' },
+  businessName: { type: String },
+  bankName: { type: String },
   bankDetails: {
-    bankCode: { type: String }, 
-    accountNumber: { type: String }, 
+    bankCode: { type: String },
+    accountNumber: { type: String },
   },
 });
 
