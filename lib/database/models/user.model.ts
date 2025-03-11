969399cd0d6b4ccc6b48fad7ffb45696d601dfee
Grëg Häris <d.gregharis@gmail.com>
2025-03-11 20:31:48 +0100

    feat: add stipe subaccountCode and Stripe Id fields for Stripe implentation

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,46 +1,23 @@
-import { Schema, model, models } from 'mongoose';
+import mongoose from 'mongoose';
 
-export interface IUser {
-  _id: string;
-  clerkId: string;
-  email: string;
-  username: string;
-  firstName: string;
-  lastName: string;
-  image: string;
-  createdAt: Date;
-  updatedAt: Date;
-  role: string;
-  businessName?: string;
-  subaccountCode?: string;
-  bankName?: string;
-  bankDetails?: {
-    bankCode?: string;
-    accountNumber?: string;
-    accountName?: string;
-  };
-}
-
-const UserSchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
-  subaccountCode: { type: String },
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  image: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  role: { type: String, default: 'user' },
-  businessName: { type: String },
-  bankName: { type: String },
-  bankDetails: {
-    bankCode: { type: String },
-    accountName: { type: String },
-    accountNumber: { type: String },
+const UserSchema = new mongoose.Schema(
+  {
+    clerkId: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    events: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }],
+    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
+    subaccountCode: { type: String, required: false },
+    businessName: { type: String },
+    bankName: { type: String },
+    bankDetails: {
+      accountNumber: { type: String },
+      accountName: { type: String },
+    },
+    stripeId: { type: String, required: false },
   },
-});
+  { timestamps: true }
+);
 
-const User = models.User || model('User', UserSchema);
-
-export default User;
+export default mongoose.models.User || mongoose.model('User', UserSchema);
