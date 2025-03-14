9e43b6cb5d94e5f234318432c180093ab245dc1f
Grëg Häris <d.gregharis@gmail.com>
2025-03-14 15:11:09 +0100

    fix: remove paymentdetails inputs for card to resolve explitly passing user payment details to Stripe error

--- components/shared/FormPaymentMethods.tsx
+++ components/shared/FormPaymentMethods.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 import { Button } from '@ui/button';
@@ -13,15 +12,13 @@ import {
   FormControl,
   FormMessage,
 } from '@ui/form';
-import { Input } from '@ui/input';
 import { PaymentMethodSelectorProps } from '@/types';
 
 export default function PaymentMethodSelector({
   isNigerianEvent,
   orderData,
 }: PaymentMethodSelectorProps) {
-  const { control, setValue, getValues } = useFormContext<checkoutFormValues>();
-  const [showCardForm, setShowCardForm] = useState(false);
+  const { control, setValue } = useFormContext<checkoutFormValues>();
 
   const handlePaystackCheckout = async () => {
     const orderWithPaystack = {
@@ -61,10 +58,7 @@ export default function PaymentMethodSelector({
                   <Button
                     type="button"
                     variant={field.value === 'card' ? 'default' : 'outline'}
-                    onClick={() => {
-                      setValue('paymentMethod', 'card');
-                      setShowCardForm(true);
-                    }}
+                    onClick={() => setValue('paymentMethod', 'card')}
                     className="rounded-md h-[40px] cursor-pointer w-full sm:w-auto"
                   >
                     Card
@@ -87,68 +81,6 @@ export default function PaymentMethodSelector({
                   >
                     Apple Pay
                   </Button>
-                </div>
-              )}
-
-              {!isNigerianEvent && showCardForm && field.value === 'card' && (
-                <div className="mt-4 space-y-4">
-                  <FormItem>
-                    <FormLabel>Card Number</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="input-field p-regular-14"
-                        placeholder="1234 5678 9012 3456"
-                        maxLength={19}
-                        onChange={(e) => {
-                          const value = e.target.value
-                            .replace(/\D/g, '')
-                            .replace(/(.{4})/g, '$1 ')
-                            .trim();
-                          e.target.value = value;
-                          setValue('cardNumber', value);
-                        }}
-                        required
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                  <div className="flex space-x-4">
-                    <FormItem className="w-1/2">
-                      <FormLabel>Expiry Date</FormLabel>
-                      <FormControl>
-                        <Input
-                          className="input-field p-regular-14"
-                          placeholder="MM/YY"
-                          maxLength={5}
-                          onChange={(e) => {
-                            let value = e.target.value.replace(/\D/g, '');
-                            if (value.length > 2)
-                              value = `${value.slice(0, 2)}/${value.slice(2)}`;
-                            e.target.value = value;
-                            setValue('expiryDate', value);
-                          }}
-                          required
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                    <FormItem className="w-1/2">
-                      <FormLabel>CVV</FormLabel>
-                      <FormControl>
-                        <Input
-                          className="input-field p-regular-14"
-                          placeholder="123"
-                          maxLength={3}
-                          onChange={(e) => {
-                            e.target.value = e.target.value.replace(/\D/g, '');
-                            setValue('cvv', e.target.value);
-                          }}
-                          required
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  </div>
                 </div>
               )}
             </div>
