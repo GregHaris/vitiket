ff5ad9ff4e7e1484e6f92cb3c2955abad2a83d13
Grëg Häris <d.gregharis@gmail.com>
2025-03-16 23:03:05 +0100

    feat: create an IUser interface

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,13 +1,32 @@
-import mongoose from 'mongoose';
+import { Document, Schema, model, models } from 'mongoose';
+export interface IUser extends Document {
+  _id: string;
+  clerkId: string;
+  email: string;
+  firstName?: string;
+  lastName?: string;
+  events: string[];
+  orders: string[];
+  subaccountCode?: string;
+  businessName?: string;
+  bankName?: string;
+  bankDetails?: {
+    accountNumber: string;
+    accountName: string;
+  };
+  stripeId?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
-const UserSchema = new mongoose.Schema(
+const UserSchema = new Schema(
   {
     clerkId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     firstName: { type: String },
     lastName: { type: String },
-    events: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }],
-    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
+    events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
+    orders: [{ type: Schema.Types.ObjectId, ref: 'Order' }],
     subaccountCode: { type: String, required: false },
     businessName: { type: String },
     bankName: { type: String },
@@ -20,4 +39,4 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.models.User || mongoose.model('User', UserSchema);
+export default models.User || model('User', UserSchema);
