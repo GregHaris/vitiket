2d4757aadb292b74f0716d56cd400e04238c2878
Grëg Häris <d.gregharis@gmail.com>
2025-03-16 23:02:00 +0100

    feat: add Account Id to remote patterns

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'c52q65crd3.ufs.sh',
+        pathname: '/f/*',
+      },
+      {
+        protocol: 'https',
         hostname: 'utfs.io',
         pathname: '/f/**',
       },
