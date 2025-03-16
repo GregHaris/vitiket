2cb759683d5a79c2a79f155599b4325e9543d7c9
Grëg Häris <d.gregharis@gmail.com>
2025-03-16 11:47:28 +0100

    feat: get the full user details using the userId and from the user details set the subaccountCode and stripeId params

--- app/(root)/create/page.tsx
+++ app/(root)/create/page.tsx
@@ -1,18 +1,24 @@
+import { getUserById } from '@/lib/actions/user.actions';
 import EventForm from '@shared/EventForm';
 import EventFormHeading from '@shared/EventFormHeading';
 import getUserId from '@/utils/userId';
 
 const CreateEvent = async () => {
   const userId = await getUserId();
+  const user = await getUserById(userId);
 
   return (
     <>
-      {' '}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <EventFormHeading/>
+        <EventFormHeading />
       </section>
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Create" />
+        <EventForm
+          userId={userId}
+          subaccountCode={user.subaccountCode}
+          stripeId={user.stripeId} 
+          type="Create"
+        />
       </div>
     </>
   );
