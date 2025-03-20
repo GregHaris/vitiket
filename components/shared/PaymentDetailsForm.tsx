e2626cc72cb0d69c6eb55b3a4fb0c2b8f06f9091
Grëg Häris <d.gregharis@gmail.com>
2025-03-20 20:00:28 +0100

    fix: remove stripeOnboarding and event hosting paymentdetails logic

--- components/shared/PaymentDetailsForm.tsx
+++ components/shared/PaymentDetailsForm.tsx
@@ -12,7 +12,6 @@ import { PaymentDetailsFormProps } from '@/types';
 import { paymentDetailsSchema } from '@/lib/validator';
 import { updateEventStatus } from '@/lib/actions/event.actions';
 import PaystackForm from './PaystackForm';
-import StripeOnboarding from './PaymentDetailsStripeOnboarding';
 
 const onSubmitSuccess = async (
   userId: string,
@@ -39,7 +38,6 @@ export default function PaymentDetailsForm({
   existingDetails: initialExistingDetails,
   eventId,
   userId,
-  isNigerianEvent,
 }: PaymentDetailsFormProps) {
   const [message, setMessage] = useState<string>('');
   const router = useRouter();
@@ -50,7 +48,6 @@ export default function PaymentDetailsForm({
     accountNumber?: string;
     accountName?: string;
     subaccountCode?: string;
-    stripeId?: string;
   } | null>(null);
 
   const form = useForm<z.infer<typeof paymentDetailsSchema>>({
@@ -74,7 +71,6 @@ export default function PaymentDetailsForm({
             accountNumber: user.bankDetails?.accountNumber || '',
             accountName: user.bankDetails?.accountName || '',
             subaccountCode: user.subaccountCode,
-            stripeId: user.stripeId,
           };
           setFetchedDetails(mappedDetails);
 
@@ -101,28 +97,15 @@ export default function PaymentDetailsForm({
         {effectiveDetails ? 'Your Payment Details' : 'Add Payment Details'}
       </h2>
       <FormProvider {...form}>
-        {isNigerianEvent ? (
-          <PaystackForm
-            banks={banks}
-            userId={userId}
-            handleSubmit={form.handleSubmit}
-            existingDetails={effectiveDetails}
-            onSubmitSuccess={() =>
-              onSubmitSuccess(userId, eventId, router, setMessage)
-            }
-          />
-        ) : (
-          <StripeOnboarding
-            userId={userId}
-            existingStripeId={effectiveDetails?.stripeId}
-            setMessage={setMessage}
-            onSubmitSuccess={() =>
-              onSubmitSuccess(userId, eventId, router, setMessage)
-            }
-            eventId={eventId}
-            existingBusinessName={effectiveDetails?.businessName} 
-          />
-        )}
+        <PaystackForm
+          banks={banks}
+          userId={userId}
+          handleSubmit={form.handleSubmit}
+          existingDetails={effectiveDetails}
+          onSubmitSuccess={() =>
+            onSubmitSuccess(userId, eventId, router, setMessage)
+          }
+        />
         {message && <p className="text-sm text-muted-foreground">{message}</p>}
       </FormProvider>
     </div>
