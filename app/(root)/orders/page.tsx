594107b5ad5aebdfa1a881966025c4601ae8883e
Grëg Häris <d.gregharis@gmail.com>
2025-03-31 07:32:37 +0100

    feat: redo the orders page

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,72 +1,72 @@
-import { getOrdersByEvent } from '@/lib/actions/order.actions';
-import { formatDateTime, formatPrice } from '@/lib/utils';
-import { IOrderItem } from '@/lib/database/models/order.model';
-import { SearchParamProps } from '@/types';
-import {NameSearch} from '@shared/SearchByName';
+import { formatDateTime, formatPrice } from "@/lib/utils";
+import { getOrdersByEvent } from "@/lib/actions/order.actions";
+import { NameSearch } from "@shared/SearchByName";
+import { IOrderItem } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
+import ExportButtons from "@shared/ExportButtons";
 
 const Orders = async ({ searchParams }: SearchParamProps) => {
   const resolvedSearchParams = await searchParams;
-  const eventId = (resolvedSearchParams.eventId as string) || '';
-  const searchText = (resolvedSearchParams.query as string) || '';
+  const eventId = (resolvedSearchParams.eventId as string) || "";
+  const searchText = (resolvedSearchParams.query as string) || "";
 
   const orders = await getOrdersByEvent({ eventId, searchString: searchText });
+
   return (
     <>
-      <section className=" bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left ">Orders</h3>
+      <section className="bg-gradient-to-r from-gray-300 to-gray-400 text-white py-8 md:py-12">
+        <h1 className="wrapper text-6xl font-bold text-center tracking-tight">
+          Orders
+        </h1>
       </section>
 
-      <section className="wrapper mt-8">
-        <NameSearch placeholder="Search buyer name..." />
+      <section className="wrapper">
+        <h2 className="wrapper text-black text-3xl font-semibold tracking-tight">
+          {orders[0].eventTitle}
+        </h2>
+        <NameSearch placeholder="Search by buyer name..." />
       </section>
 
       <section className="wrapper overflow-x-auto">
-        <table className="w-full border-collapse border-t">
+        <div className="text-right sticky top-0 bg-white z-10">
+          <ExportButtons orders={orders} />
+        </div>
+        <table className="w-full border-collapse border-r border-l border bg-white">
           <thead>
-            <tr className="p-medium-14 border-b text-grey-500">
-              <th className="min-w-[250px] py-3 text-left">Order ID</th>
-              <th className="min-w-[200px] flex-1 py-3 pr-4 text-left">
-                Event Title
-              </th>
-              <th className="min-w-[150px] py-3 text-left">Buyer</th>
-              <th className="min-w-[200px] py-3 text-left">Buyer Email</th>
-              <th className="min-w-[100px] py-3 text-left">Created</th>
-              <th className="min-w-[100px] py-3 text-right">Amount</th>
+            <tr className="bg-gray-700 text-white text-sm uppercase tracking-wider">
+              <th className="py-4 pl-6 text-left">Order ID</th>
+              <th className="py-4 pl-6 text-left">Buyer</th>
+              <th className="py-4 pl-6 text-left">Buyer Email</th>
+              <th className="py-4 pl-6 text-left">Created</th>
+              <th className="py-4 pr-6 text-right">Amount</th>
             </tr>
           </thead>
           <tbody>
             {orders && orders.length === 0 ? (
-              <tr className="border-b">
-                <td colSpan={6} className="py-4 text-center text-gray-500">
+              <tr>
+                <td colSpan={5} className="py-6 text-center text-gray-500">
                   No orders found.
                 </td>
               </tr>
             ) : (
-              <>
-                {orders &&
-                  orders.map((row: IOrderItem) => (
-                    <tr
-                      key={row._id}
-                      className="p-regular-14 lg:p-regular-16 border-b "
-                      style={{ boxSizing: 'border-box' }}
-                    >
-                      <td className="min-w-[250px] py-4 text-primary-500">
-                        {row._id}
-                      </td>
-                      <td className="min-w-[200px] flex-1 py-4 pr-4">
-                        {row.eventTitle}
-                      </td>
-                      <td className="min-w-[150px] py-4">{row.buyer}</td>
-                      <td className="min-w-[200px] py-4">{row.buyerEmail}</td>
-                      <td className="min-w-[100px] py-4">
-                        {formatDateTime(row.createdAt).dateTime}
-                      </td>
-                      <td className="min-w-[100px] py-4 text-right">
-                        {formatPrice(row.totalAmount)}
-                      </td>
-                    </tr>
-                  ))}
-              </>
+              orders.map((row: IOrderItem) => (
+                <tr
+                  key={row._id}
+                  className="border-b hover:bg-gray-50 transition-colors"
+                >
+                  <td className="py-4 pl-6 text-primary font-medium">
+                    {row._id}
+                  </td>
+                  <td className="py-4 pl-6">{row.buyer}</td>
+                  <td className="py-4 pl-6 break-words">{row.buyerEmail}</td>
+                  <td className="py-4 pl-6">
+                    {formatDateTime(row.createdAt).dateTime}
+                  </td>
+                  <td className="py-4 pr-6 text-right font-medium">
+                    {formatPrice(row.totalAmount)}
+                  </td>
+                </tr>
+              ))
             )}
           </tbody>
         </table>
