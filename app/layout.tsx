8f5df36d117b3d39b7c04c894ec5cdc94368c841
Grëg Häris <d.gregharis@gmail.com>
2025-03-31 09:55:15 +0100

    feat: import and use Vercel's Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import { ClerkProvider } from '@clerk/nextjs';
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+import type { Metadata } from "next";
 
-import './globals.css';
+import "./globals.css";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  variable: '--font-poppins',
-  weight: ['400', '500', '600', '700'],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-  title: 'Vitiket',
-  description: 'An Event management app',
-  icons: '/assets/images/logo.png',
+  title: "Vitiket",
+  description: "An Event management app",
+  icons: "/assets/images/logo.png",
 };
 
 export default function RootLayout({
@@ -25,6 +26,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${poppins.variable}antialiased`}>{children}</body>
+        <Analytics />
       </html>
     </ClerkProvider>
   );
