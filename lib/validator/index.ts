b0434f0526fbe38aa411d46ff890497a8d6d9d87
Grëg Häris <d.gregharis@gmail.com>
2025-04-08 12:33:02 +0100

    feat: remove profileUpdateSchema and Values

--- lib/validator/index.ts
+++ lib/validator/index.ts
@@ -220,11 +220,3 @@ export const paymentDetailsSchema = z.object({
 });
 
 export type PaymentDetailsValues = z.infer<typeof paymentDetailsSchema>;
-
-export const profileUpdateFormSchema = z.object({
-  firstName: z.string().min(1, "First name is required"),
-  lastName: z.string().min(1, "Last name is required"),
-  username: z.string().min(1, "Username is required"),
-});
-
-export type ProfileUpdateFormValues = z.infer<typeof profileUpdateFormSchema>;
