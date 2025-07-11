8c10101c62a6caecd69e7b9333c457e560f4bc6d
Greg Haris <d.gregharis@gmail.com>
2025-07-11 04:22:22 +0100

    style: add neutral black and white colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,103 +1,102 @@
 /** @type {import('tailwindcss').Config} */
-import animate from 'tailwindcss-animate';
-import typography from '@tailwindcss/typography';
+import animate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
 const config = {
   important: true,
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    // Add UploadThing UI paths
-    './node_modules/@uploadthing/react/dist**',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./node_modules/@uploadthing/react/dist**",
   ],
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: "2rem",
       screens: {
-        '2xl': '1400px',
+        "2xl": "1400px",
       },
     },
     extend: {
       colors: {
         primary: {
-          50: '#e6f0f8',
-          100: '#cddfeb',
-          200: '#9bbfd7',
-          300: '#6a9fc3',
-          400: '#3870af',
-          500: '#123f70',
-          600: '#0f335a',
-          700: '#0c2744',
-          800: '#091b2e',
-          900: '#060f18',
-          DEFAULT: '#123f70',
-          foreground: 'hsl(var(--primary-foreground))',
+          50: "#e6f0f8",
+          100: "#cddfeb",
+          200: "#9bbfd7",
+          300: "#6a9fc3",
+          400: "#3870af",
+          500: "#123f70",
+          600: "#0f335a",
+          700: "#0c2744",
+          800: "#091b2e",
+          900: "#060f18",
+          DEFAULT: "#123f70",
+          foreground: "hsl(var(--primary-foreground))",
         },
         grey: {
-          600: '#545454',
-          500: '#757575',
-          400: '#AFAFAF',
-          50: '#F6F6F6',
+          600: "#545454",
+          500: "#757575",
+          400: "#AFAFAF",
+          50: "#F6F6F6",
         },
-        black: '#36454F',
-        white: '#FFFFFF',
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        foreground: 'hsl(var(--foreground))',
+        "neutral-black": "#0C1314",
+        "neutral-white": "#FDFDFD",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        foreground: "hsl(var(--foreground))",
         secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
         destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
         muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
         accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
         popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
         card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
       },
       fontFamily: {
-        poppins: ['var(--font-poppins)'],
+        poppins: ["var(--font-poppins)"],
       },
       backgroundImage: {
-        'dotted-pattern': "url('/assets/images/dotted-pattern.png')",
-        'hero-img': "url('/assets/images/hero.png')",
+        "dotted-pattern": "url('/assets/images/dotted-pattern.png')",
+        "hero-img": "url('/assets/images/hero.png')",
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
