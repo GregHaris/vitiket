ce200b27152534dc3f889e9cf7a0147d1b5cf940
Greg Haris <d.gregharis@gmail.com>
2025-07-11 04:26:03 +0100

    fix: change from h to min-h

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import Header from '@shared/Header';
-import Footer from '@shared/Footer';
+import Header from "@shared/Header";
+import Footer from "@shared/Footer";
 
 export default function RootLayout({
   children,
@@ -7,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">{children}</main>
       <Footer />
