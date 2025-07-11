5c8f13b0072dcf0b80be4e1d4bbc367c3d822afc
Greg Haris <d.gregharis@gmail.com>
2025-07-11 04:47:53 +0100

    style: add footerLinks styles

--- components/shared/Footer/FooterLinks.tsx
+++ components/shared/Footer/FooterLinks.tsx
@@ -11,10 +11,8 @@ export const LinkList = ({
     <h4 className="font-semibold mb-4 text-gray-400">{title}</h4>
     <ul className="space-y-3 text-sm text-gray-200">
       {links.map((link) => (
-        <li key={link}>
-          <Link href="#" className="hover:text-white">
-            {link}
-          </Link>
+        <li key={link} className="footerLinks">
+          <Link href="#">{link}</Link>
         </li>
       ))}
     </ul>
